8eda1918c4eae299fd0487cebaf9f24a93b0ecb1
Scott Redig <Laremere@gmail.com>
2014-11-12 19:22:45 -0600

    Fixed checkstyle errors

---
+++ src/edu/umn/d/grenoble/mhcs/client/MarsHabitatConfigurationSystem.java
@@ -0,0 +1,15 @@
+package edu.umn.d.grenoble.mhcs.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class MarsHabitatConfigurationSystem implements EntryPoint {
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+    }
+}
