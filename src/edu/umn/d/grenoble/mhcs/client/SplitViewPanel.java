d4880b374c7c9571f6049656e0d05d9944fbb404
Paul Rodysill <rodys009@d.umn.edu>
2014-12-10 17:48:43 -0600

    Added User Story: Size of moving task

--- src/edu/umn/d/grenoble/mhcs/client/SplitViewPanel.java
+++ src/edu/umn/d/grenoble/mhcs/client/SplitViewPanel.java
@@ -1,16 +1,19 @@
 package edu.umn.d.grenoble.mhcs.client;
 
+import java.util.List;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 import edu.umn.d.grenoble.mhcs.bus.AreaUpdateEvent;
 import edu.umn.d.grenoble.mhcs.bus.Bus;
 import edu.umn.d.grenoble.mhcs.modules.Area;
+import edu.umn.d.grenoble.mhcs.modules.Module;
 
 public class SplitViewPanel extends Tab {
     
@@ -18,6 +21,7 @@ public class SplitViewPanel extends Tab {
     ListBox listBox2;
     Button submitButton;
     FlowPanel splitViewPanel;
+    Label label;
     
     /**
      * Constructor for SplitViewPanel
@@ -26,6 +30,7 @@ public class SplitViewPanel extends Tab {
         this.listBox1 = new ListBox();
         this.listBox2 = new ListBox();
         this.submitButton = new Button("Submit");
+        this.label = new Label(" ");
         
         this.splitViewPanel = new FlowPanel();
 
@@ -33,6 +38,7 @@ public class SplitViewPanel extends Tab {
         this.splitViewPanel.add(this.listBox1);
         this.splitViewPanel.add(this.listBox2);
         this.splitViewPanel.add(this.submitButton);
+        this.splitViewPanel.add(this.label);
         
         final SplitViewPanel splitViewPanel = this;
         
@@ -47,6 +53,7 @@ public class SplitViewPanel extends Tab {
                 //Gets appropriate areas from list and fires event.
                 Area area1 = AreaHolder.getArea(areas[splitViewPanel.listBox1.getSelectedIndex()]);
                 Area area2 = AreaHolder.getArea(areas[splitViewPanel.listBox2.getSelectedIndex()]);
+                label.setText( "Size of moving task: " + ( compare(area1, area2) ) * 5 + " meters" );
                 Bus.bus.fireEvent( new AreaUpdateEvent( area1, area2 ));
             }
         });
@@ -70,7 +77,7 @@ public class SplitViewPanel extends Tab {
         
         String[] areas = AreaHolder.getAreas();
         if(areas.length == 0){
-            this.listBox1.addItem("Nothing ");
+            this.listBox1.addItem("Nothing");
             this.listBox2.addItem("Nothing");
         }
         //Cycles through list of areas and adds to list boxes
@@ -89,5 +96,21 @@ public class SplitViewPanel extends Tab {
         return "Split View";
     }
     
+    private int compare(Area area1, Area area2) {
+        List<Module> list1 = area1.getModules();
+        List<Module> list2 = area2.getModules();
+        int distance = 0;
+        
+        for ( Module m1 : list1 ) {
+            for ( Module m2 : list2 ) {
+                if ( m1.getId() == m2.getId() ) {
+                    distance += Math.abs( m1.getX() - m2.getX() );
+                    distance += Math.abs( m1.getY() - m2.getY() );
+                }
+            }
+        }
+        
+        return distance;    
+    }
     
 }
