7a8720c4d763979f7e58818a45b563f7fbebb5dd
Paul Rodysill <rodys009@d.umn.edu>
2014-12-11 03:11:23 -0600

    Overhauled WeatherPanel.java   Now correctly displays the sunset time and removed phantom quotation marks

--- src/edu/umn/d/grenoble/mhcs/client/WeatherPanel.java
+++ src/edu/umn/d/grenoble/mhcs/client/WeatherPanel.java
@@ -21,124 +21,139 @@ public class WeatherPanel {
     
  /* Initialization */
     
-    private final JsonpRequestBuilder jsonp;
-    private final JsonpRequestBuilder jsonp1;
-    private FlowPanel weatherTable;
-    private String visString;
-    private String tempString;
-    private Image wunderLogo;
-    private Label weatherHeader;
-    private String stringTemp;
-    private String stringVis;
-    private String stringHour;
-    private String stringMinute;
+    private final JsonpRequestBuilder jsonp = new JsonpRequestBuilder();
+    private final String url = URL.encode("http://api.wunderground.com/api/8fd09796b6c9895c/conditions/q/55812.json?");
+    private final String url2 = URL.encode("http://api.wunderground.com/api/8fd09796b6c9895c/astronomy/q/55812.json?");
+    private FlowPanel weatherPanel  = new FlowPanel();
+    private Image wunderLogo = new Image();
+    private String temperature;
+    private String visibility;
+    private int hoursUntilSunset;
+    private int minutesUntilSunset;
+    private boolean ready;
   
     /**
      * The WeatherPanel Constructor.
      * Obtains a JSON Object containing the current weather conditions
      * and creates a panel to display those conditions.
      */
-    public WeatherPanel(){
+    public WeatherPanel() {     
         
-        String url= URL.encode("http://api.wunderground.com/api/8fd09796b6c9895c/conditions/q/55812.json?");
-        String url2 = URL.encode("http://api.wunderground.com/api/8fd09796b6c9895c/astronomy/q/55812.json?");
-        
-        
-        //Next two sections are performing JSON request to wunderground servers and calling update methods.
-        this.jsonp = new JsonpRequestBuilder();
-        this.jsonp.setCallbackParam("callback");
-        this.jsonp.requestObject(url,  new AsyncCallback<JavaScriptObject>() {
-            public void onFailure(final Throwable caught){
-                Window.alert("Json onFailure");
-            }
-          
-            @Override          
-            public void onSuccess(final JavaScriptObject s) {
-                JSONObject obj = new JSONObject(s);
-                update( obj.toString() );              
-            }     
-        });
-    
-        this.jsonp1 = new JsonpRequestBuilder();
-        this.jsonp1.setCallbackParam("callback");        
-        this.jsonp1.requestObject(url2,  new AsyncCallback<JavaScriptObject>() {
-            public void onFailure(final Throwable caught){
-                Window.alert("Json onFailure");
-            }
-          
-            @Override          
-            public void onSuccess(final JavaScriptObject s) {
-                JSONObject obj = new JSONObject(s);
-                update2( obj.toString() );              
-            }     
-        });
-        
-        this.weatherTable = new FlowPanel();
-        this.weatherTable.setStyleName("weatherPanel");
-        this.visString = new String("Visibility: ");
-        this.tempString = new String("Temperature:  ");
-        
-        this.wunderLogo = new Image();
+        this.weatherPanel.setStyleName("weatherPanel");
         this.wunderLogo.setUrl("images/WunderGroundLogo.jpg");
         
-        this.weatherHeader = new Label("Weather Conditions:");
-        
-        
-
+        // Gets the astronomical data from wunderground
+        this.jsonp.requestObject(this.url2 ,  new AsyncCallback<JavaScriptObject>() {
+            
+            /**
+             * If data retrieval is successful, the data gets processed in the updateSunset method.
+             * NOTE: This class overrides AsyncCallback's onSuccess method
+             */
+            @Override          
+            public void onSuccess(final JavaScriptObject s) {
+                updateSunset( ( new JSONObject(s) ).toString() );
+                if (ready) { create(); } else { ready = true; }
+            }
+            
+            /**
+             * If data retrieval is fails, an error message is displayed.
+             * NOTE: This class overrides AsyncCallback's onFailure method
+             */
+            @Override
+            public void onFailure(final Throwable caught){
+                Window.alert("Error: Problem recieving JSON for sunset from wunderground");
+            }     
+        });
+            
+        // Gets the weather data from wunderground
+        this.jsonp.requestObject(this.url ,  new AsyncCallback<JavaScriptObject>() {
+          
+            /**
+             * If data retrieval is successful, the data gets processed in the updateWeather method.
+             * NOTE: This class overrides AsyncCallback's onSuccess method
+             */
+            @Override          
+            public void onSuccess(final JavaScriptObject s) {
+                updateWeather( ( new JSONObject(s) ).toString() );
+                if (ready) { create(); } else { ready = true; }
+            }     
+            
+            /**
+             * If data retrieval is fails, an error message is displayed.
+             * NOTE: This class overrides AsyncCallback's onFailure method
+             */
+            @Override
+            public void onFailure(final Throwable caught){
+                Window.alert("Error: Problem recieving JSON for weather conditions from wunderground");
+            }
+        });       
     }
     
- /* Methods */
+ /* Getter */
   
-    /**
-     * Getter (Accessor) for the weather panel.
-     */
-    public FlowPanel getWeatherPanel() {
-        return this.weatherTable;
-    }
+    public FlowPanel getWeatherPanel() { return this.weatherPanel; }
+    
+ /* Private Methods */
     
     /**
      * Parses a JSONObject for relevant values then 
      * adds the values to the weatherTable.
-     * @param conditions_ - The current weather conditions
+     * @param conditions - The current weather conditions
      */
-    public void update(final String conditions_){
+    private void updateWeather(final String conditions) {
         
-        JSONObject conditions = (JSONObject) JSONParser.parseLenient(conditions_);
-        String current = conditions.get("current_observation").toString();
-        conditions = (JSONObject) JSONParser.parseLenient( current );
+        JSONObject currentObject = (JSONObject) JSONParser.parseLenient(conditions);
+        currentObject = this.fetchObject(currentObject, "current_observation");
         
-        this.stringTemp = conditions.get("temp_c").toString();
-        this.stringVis = conditions.get("visibility_km").toString();
-        
-        
-        
+        this.temperature = currentObject.get("temp_c").toString();
+        this.visibility = currentObject.get("visibility_km").toString().replace("\"", "");
 
-        this.weatherTable.add(this.weatherHeader);
-        this.weatherTable.add(new Label(this.tempString + "  " + this.stringTemp + " Degrees C"));
-        this.weatherTable.add(new Label(this.visString + " " + this.stringVis + " Km"));
-        this.weatherTable.add(new Label("Sunset time: " + this.stringHour + ":" + this.stringMinute));
-        this.weatherTable.add(this.wunderLogo);
-        
-  
     }
-    
+        
+    /**
+     * Parses a JSONObject for the sunset time.
+     * @param astronomy - The current astronomical conditions
+     */
+    private void updateSunset(final String astronomy) {
+        
+        JSONObject currentObject = (JSONObject) JSONParser.parseLenient(astronomy);
+        
+        currentObject = this.fetchObject(currentObject, "sun_phase");
+        currentObject = this.fetchObject(currentObject, "sunset");
+        
+        this.hoursUntilSunset = this.fetchInt(currentObject, "hour") - 12;
+        this.minutesUntilSunset = this.fetchInt(currentObject, "minute");       
+
+    }  
     
     /**
-     * Parses a JSONObject for relevant values then 
-     * adds the values to the weatherTable.
-     * @param conditions_ - The current weather conditions
+     * Creates the weather panel once all data is loaded.
      */
-    public void update2(final String astronomy_) {
-        JSONObject astronomy = (JSONObject) JSONParser.parseLenient(astronomy_);
-        String current = astronomy.get("sun_phase").toString();
-        astronomy = (JSONObject) JSONParser.parseLenient(current);
-        
-        current = astronomy.get("sunset").toString();
-        astronomy = (JSONObject) JSONParser.parseLenient(current);
-        
-        this.stringHour = astronomy.get("hour").toString();
-        this.stringMinute = astronomy.get("minute").toString();
-
+    private void create() {      
+        this.weatherPanel.add( new Label("Weather Conditions:") );
+        this.weatherPanel.add( new Label("Temperature: " + this.temperature + " \u00b0C") );
+        this.weatherPanel.add( new Label("Visibility: " + this.visibility + " Km") );
+        this.weatherPanel.add( new Label("Sunset time: " + this.hoursUntilSunset + ":" + this.minutesUntilSunset + " PM ") );
+        this.weatherPanel.add(this.wunderLogo); 
     }
     
+    /**
+     * Parses a JSONObject for the key and returns the integer value stored at that key.
+     * @param obj - The JSON Object that is being parsed
+     * @param key - The key that is being searched for
+     * @return The integer that was associated with the key
+     */
+    private int fetchInt( final JSONObject currentObject, final String key) {
+        return Integer.parseInt( currentObject.get("hour").toString().replace("\"", "") );
+    }
+    
+    /**
+     * Parses a JSONObject for the key and returns the object stored at that key.
+     * @param obj - The JSON Object that is being parsed
+     * @param key - The key that is being searched for
+     * @return The new JSON Object that was associated with the key
+     */
+    private JSONObject fetchObject(final JSONObject obj, final String key) {
+        return (JSONObject) JSONParser.parseLenient( obj.get(key).toString() );
+    }
 }
